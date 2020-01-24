83f7d597da29dc7436003dd1c01fdb75110ac677
abeidahmed <abeidahmed92@gmail.com>
2020-01-24 19:32:07 +0530

    Add styling to section hues

--- src/components/layouts/global/SectionHues.js
+++ src/components/layouts/global/SectionHues.js
@@ -1,20 +1,42 @@
 import React from "react";
 import shortid from "shortid";
 
-function huesOpen(props) {
+const huesOpen = props => {
+  const textColor = {
+    color: props.colorChange["Text color"]
+  };
+
+  const headingColor = {
+    color: props.colorChange["Heading color"]
+  };
+
   if (props.huesOpen) {
     return (
       <div className="section-hues">
-        <h3 className="section-hues-title">This section's hues</h3>
-        <p className="section-hues-subtitle">Click to copy the hex code.</p>
+        <h3 style={headingColor} className="section-hues-title">
+          This section's hues
+        </h3>
+        <p style={textColor} className="section-hues-subtitle">
+          Click to copy the hex code.
+        </p>
         <div className="section-hues-container">
           {Object.entries(props.colorChange).map(([colorName, color]) => (
             <div key={shortid.generate()} className="button-hues-container">
               <button className="button-hues">
-                <span style={{ backgroundColor: color }} className="hues-display" />
+                <span
+                  style={{
+                    backgroundColor: color,
+                    borderColor: props.colorChange["Text color"]
+                  }}
+                  className="hues-display"
+                />
                 <span className="hues-info">
-                  <span className="section-color-name">{colorName}</span>
-                  <span className="color-hex">{color.toLowerCase()}</span>
+                  <span style={textColor} className="section-color-name">
+                    {colorName}
+                  </span>
+                  <span style={textColor} className="color-hex">
+                    {color.toLowerCase()}
+                  </span>
                 </span>
               </button>
             </div>
@@ -25,7 +47,7 @@ function huesOpen(props) {
   } else {
     return null;
   }
-}
+};
 
 const SectionHues = props => {
   return huesOpen(props);
