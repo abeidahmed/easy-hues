16b8417bdaef79afd16ea1daf9e0d8e5c20a5634
abeidahmed <abeidahmed92@gmail.com>
2020-01-24 21:27:20 +0530

    Add featured image

--- src/components/layouts/hero/Hero.js
+++ src/components/layouts/hero/Hero.js
@@ -56,6 +56,56 @@ const Hero = props => {
                 </p>
               </div>
             </div>
+            <div className="hero-right">
+              <div className="hero-right-inner">
+                <img
+                  src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+                  alt="meeting"
+                />
+                <svg width="114" height="114" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g opacity=".9" filter="url(#filter0_dd)">
+                    <circle cx="57" cy="49" r="42" fill="#fff" />
+                  </g>
+                  <path
+                    opacity=".9"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M48.288 34.313a2.5 2.5 0 012.537.067l20 12.5a2.5 2.5 0 010 4.24l-20 12.5A2.5 2.5 0 0147 61.5v-25a2.5 2.5 0 011.288-2.187z"
+                    fill={props.colorChange["Heading accent color"]}
+                  />
+                  <defs>
+                    <filter
+                      id="filter0_dd"
+                      x="0"
+                      y="0"
+                      width="114"
+                      height="114"
+                      filterUnits="userSpaceOnUse"
+                      colorInterpolationFilters="sRGB"
+                    >
+                      <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix
+                        in="SourceAlpha"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                      />
+                      <feOffset dy="3" />
+                      <feGaussianBlur stdDeviation="3" />
+                      <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+                      <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+                      <feColorMatrix
+                        in="SourceAlpha"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                      />
+                      <feOffset dy="8" />
+                      <feGaussianBlur stdDeviation="7.5" />
+                      <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+                      <feBlend in2="effect1_dropShadow" result="effect2_dropShadow" />
+                      <feBlend in="SourceGraphic" in2="effect2_dropShadow" result="shape" />
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
+            </div>
           </div>
           <SectionHues huesOpen={props.huesOpen} colorChange={props.colorChange} />
         </div>
