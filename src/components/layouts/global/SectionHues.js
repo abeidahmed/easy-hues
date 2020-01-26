cec7b27d4afc372ae7708362ab3c8713dccb24b9
abeidahmed <abeidahmed92@gmail.com>
2020-01-26 14:25:32 +0530

    Add click to copy functionality

--- src/components/layouts/global/SectionHues.js
+++ src/components/layouts/global/SectionHues.js
@@ -1,66 +1,101 @@
-import React from "react";
+import React, { Component } from "react";
 import shortid from "shortid";
 
-const huesOpen = props => {
-  const { heading, text } = props.colorChange;
-
-  const camelCaseToWord = word => {
-    const toArray = word.replace(/([A-Z])/g, " $1").toLowerCase();
-    const splitArray = toArray.split(" ");
-    return splitArray[0][0].toUpperCase() + toArray.slice(1);
+class SectionHues extends Component {
+  state = {
+    value: "",
+    copied: false
   };
 
-  // Check if inherited colors has it's own property.
-  // Return heading color if found else return text color.
-  const propCheck = (property, element) => {
-    if (property.hasOwnProperty(element)) {
-      return heading;
-    } else {
-      return text;
+  onClick = async hexCode => {
+    let mark = "";
+    this.setState({ value: hexCode });
+
+    mark = await document.createElement("input");
+    mark.value = this.state.value;
+
+    document.body.appendChild(mark);
+    mark.select();
+    document.execCommand("copy");
+
+    this.setState({ copied: true });
+
+    if (this.state.copied) {
+      document.body.removeChild(mark);
     }
+
+    await this.toggleFalse();
   };
 
-  if (props.huesOpen) {
-    return (
-      <div className="section-hues">
-        <h3 style={{ color: propCheck(props.colorChange, heading) }} className="section-hues-title">
-          This section's hues
-        </h3>
-        <p style={{ color: text }} className="section-hues-subtitle">
-          Click to copy the hex code.
-        </p>
-        <div className="section-hues-container">
-          {Object.entries(props.colorChange).map(([colorName, colorHex]) => (
-            <div key={shortid.generate()} className="button-hues-container">
-              <button className="button-hues">
-                <span
-                  style={{
-                    backgroundColor: colorHex,
-                    borderColor: text
-                  }}
-                  className="hues-display"
-                />
-                <span className="hues-info">
-                  <span style={{ color: text }} className="section-color-name">
-                    {camelCaseToWord(colorName)}
+  toggleFalse = () => {
+    setTimeout(() => {
+      this.setState({ copied: false });
+    }, 3000);
+  };
+
+  render() {
+    const { heading, text } = this.props.colorChange;
+
+    const camelCaseToWord = word => {
+      const toArray = word.replace(/([A-Z])/g, " $1").toLowerCase();
+      const splitArray = toArray.split(" ");
+      return splitArray[0][0].toUpperCase() + toArray.slice(1);
+    };
+
+    // Check if inherited colors has it's own property.
+    // Return heading color if found else return text color.
+    const propCheck = (property, element) => {
+      if (property.hasOwnProperty(element)) {
+        return heading;
+      } else {
+        return text;
+      }
+    };
+
+    if (this.props.huesOpen) {
+      return (
+        <div className="section-hues">
+          <h3
+            style={{ color: propCheck(this.props.colorChange, heading) }}
+            className="section-hues-title"
+          >
+            This section's hues
+          </h3>
+          <p style={{ color: text }} className="section-hues-subtitle">
+            Click to copy the hex code.
+          </p>
+          <div className="section-hues-container">
+            {Object.entries(this.props.colorChange).map(([colorName, colorHex]) => (
+              <div key={shortid.generate()} className="button-hues-container">
+                <button
+                  onClick={() => this.onClick(colorHex.toLowerCase())}
+                  className="button-hues"
+                >
+                  <span
+                    style={{
+                      backgroundColor: colorHex,
+                      borderColor: text
+                    }}
+                    className="hues-display"
+                  />
+                  <span className="hues-info">
+                    <span style={{ color: text }} className="section-color-name">
+                      {camelCaseToWord(colorName)}
+                    </span>
+                    <span style={{ color: text }} className="color-hex">
+                      {colorHex.toLowerCase()}
+                    </span>
                   </span>
-                  <span style={{ color: text }} className="color-hex">
-                    {colorHex.toLowerCase()}
-                  </span>
-                </span>
-              </button>
-            </div>
-          ))}
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    );
-  } else {
+      );
+    }
+
     return null;
   }
-};
-
-const SectionHues = props => {
-  return huesOpen(props);
-};
+}
 
 export default SectionHues;
